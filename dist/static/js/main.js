aed1a18ac7d6e73dd70fdbbcd1f1f0fb32306deb
puge <mail@puge.org>
2021-02-20 23:01:19 +0800

    save

--- dist/static/js/main.js
+++ dist/static/js/main.js
@@ -1,10 +1,10 @@
-// window.autoScaleOnLoad = function () {
-//   autoScale({
-//     // 设计宽度PC
-//     deviseW: 1920,
-//     // 设计高度PC
-//     deviseH: 3561,
-//     type: 'show',
-//     box: '.show'
-//   })
-// }
+window.autoScaleOnLoad = function () {
+  autoScale({
+    // 设计宽度PC
+    deviseW: 1920,
+    // 设计高度PC
+    deviseH: 5384,
+    type: 'show',
+    box: '.show'
+  })
+}
